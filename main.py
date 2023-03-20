b928ab7a6f06d770bf32655e753d3bfdf1c556ab
fallnomega <fallnomega@gmail.com>
2023-03-20 16:06:36 -0700

    importing selenium webdriver

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+from selenium import webdriver
+
+chrome_driver = 'chromedriverDirectory'
+
+driver = webdriver.Chrome(executable_path=chrome_driver)
+driver.get('https://www.amazon.com')
+# driver.close() #clses the tab the driver opens
+# driver.quit() # closes out the browser.
