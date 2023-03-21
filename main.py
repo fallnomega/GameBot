0578d0e0dec5796f8165d34fed94c11c75a7106c
fallnomega <fallnomega@gmail.com>
2023-03-21 12:17:58 -0700

    getting cookie clicking option going

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
 
+options = Options()
+options.add_experimental_option("detach",True)
 chrome_driver = 'chromedriverDirectory'
+driver = webdriver.Chrome(executable_path=chrome_driver, options=options)
+driver.get('http://orteil.dashnet.org/experiments/cookie/')
+# language = driver.find_element(By.XPATH,'//*[@id="langSelect-EN"]')
+the_cookie = driver.find_element(By.ID,'cookie')
+the_cookie.click()
+# print(language.text)
+# the_cookie = driver.find_element(By.XPATH,'//*[@id="bigCookie"]')
 
-driver = webdriver.Chrome(executable_path=chrome_driver)
-driver.get('https://www.amazon.com')
 # driver.close() #clses the tab the driver opens
-# driver.quit() # closes out the browser.
+
+# for x in range(100):
+#     the_cookie.click()
+
