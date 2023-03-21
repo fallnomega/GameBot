cc7f865110a9bfab545d98e232235fd2c879c86b
fallnomega <fallnomega@gmail.com>
2023-03-21 14:00:23 -0700

    implemented auto buying upgrades based on max upgrade i can afford

--- main.py
+++ main.py
@@ -3,20 +3,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+import re
+
+
+def find_max_item_to_buy(the_options):
+    name = []
+    value = []
+    for x in my_choices:
+        if my_money >= float(x[1].replace(',', '')):
+            name.append(x[0])
+            value.append(int(x[1].replace(',', '')))
+    max_value = max(value)
+    max_location = value.index(max_value)
+    return [name[max_location], value[max_location]]
+
 
 options = Options()
-options.add_experimental_option("detach",True)
+options.add_experimental_option("detach", True)
 chrome_driver = 'chromedriverDirectory'
 driver = webdriver.Chrome(executable_path=chrome_driver, options=options)
 driver.get('http://orteil.dashnet.org/experiments/cookie/')
-# language = driver.find_element(By.XPATH,'//*[@id="langSelect-EN"]')
-the_cookie = driver.find_element(By.ID,'cookie')
+the_cookie = driver.find_element(By.ID, 'cookie')
 the_cookie.click()
-# print(language.text)
-# the_cookie = driver.find_element(By.XPATH,'//*[@id="bigCookie"]')
 
-# driver.close() #clses the tab the driver opens
+t_end = time.time() + 2
+while time.time() < t_end:
+    the_cookie.click()
+time.sleep(2)
 
-# for x in range(100):
-#     the_cookie.click()
+my_money = driver.find_element(By.ID, 'money')
+my_money = int(my_money.text)
+print(f"money available: {my_money}")
+
+buy_options = driver.find_element(By.ID, 'store')
+temp = buy_options.text.split('\n')
+
+selections = [re.split('\n.*\n', x) for x in temp]
+del selections[1::2]
+my_choices = [x[0].split(' - ') for x in selections]
+buying = find_max_item_to_buy(my_choices)
+print(buying)
+get_upgrade= driver.find_element(By.ID, f'buy{buying[0]}')
+get_upgrade.click()
 
