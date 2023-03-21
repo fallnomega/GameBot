a97d74d2ce669ab57fc5eb9837529c77964e66ce
fallnomega <fallnomega@gmail.com>
2023-03-21 14:41:45 -0700

    formatting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import re
 
 
-def find_max_item_to_buy(the_options,my_bank):
+def find_max_item_to_buy(the_options, my_bank):
     name = []
     value = []
     for x in the_options:
@@ -15,6 +15,7 @@ def find_max_item_to_buy(the_options,my_bank):
     max_value = max(value)
     max_location = value.index(max_value)
     return [name[max_location], value[max_location]]
+
 
 options = Options()
 options.add_experimental_option("detach", True)
@@ -37,14 +38,14 @@ while keep_alive:
         the_cookie.click()
     time.sleep(2)
     my_money = driver.find_element(By.ID, 'money')
-    my_money = my_money.text.replace(',','')
+    my_money = my_money.text.replace(',', '')
     my_money = int(my_money)
-    buying = find_max_item_to_buy(my_choices,my_money)
+    buying = find_max_item_to_buy(my_choices, my_money)
     get_upgrade = driver.find_element(By.ID, f'buy{buying[0]}')
     get_upgrade.click()
     sentinal += 1
     if sentinal >= 5:
         keep_alive = False
 time.sleep(2)
-cookies_per_second = driver.find_element(By.ID,'cps')
+cookies_per_second = driver.find_element(By.ID, 'cps')
 print(f"cookies per second: {cookies_per_second.text}")
